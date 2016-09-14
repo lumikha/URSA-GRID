aaeed0b2e6267b507d30df59e5b1858cff596f2a
Bethany Joyce Sibala <bethany@lumikha.co>
2016-09-14 16:41:43 +0800

    done with dashboard.php
    good to go :)

--- dashboard.php
+++ dashboard.php
@@ -1,44 +1,51 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <link rel="stylesheet" href="../style.css">
+ <!--960 grid stylesheet links-->
 
+    <link href="css/960.css" rel="stylesheet"/>
+    <link href="css/reset.css" rel="stylesheet"/>
+    <link href="css/text.css" rel="stylesheet"/>
+     <link rel="stylesheet" href="style.css"/>
 
+<div class="full-width-div">
+<div class= "container_12" style="margin-top:5em;">
     <form id="dashboard_form">
-        <div class="row">
-            <div class="col-md-6">
+        <div class="container_12">
+            <div class="grid_6 alpha">
                 <input type="text" class="form-control" placeholder="Username">
             </div>
-            <div class="col-md-6">
+            <div class="grid_6 omega">
                 <input type="text" class="form-control" placeholder="Password">
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-9">
+        <div class="container_12">
+            <div class="grid_9 alpha">
                  <select class="form-control">
                      <option value="" disabled selected>Recovery Question</option>
                      <option>What's the name of your first pet?</option>
                      <option>Who's your favorite Celebrity?</option>
                  </select>
             </div>
-            <div class="col-md-3">
+            <div class="grid_3 omega">
                  <input type="text" class="form-control" placeholder="Recovery Answer">
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6">
+        <div class="container_12">
+            <div class="grid_6 alpha">
                 <select class="form-control">
                      <option value="" disabled selected>Notification Preference</option>
                      <option>Preference 1</option>
                      <option>Preference 2</option>
                  </select>
             </div>
-            <div class="col-md-6">
+            <div class="grid_4 omega">
                 <label>Turn On Two-Step Access?</label>
                 <label class="radio-inline"><input type="radio" name="two_step" value="yes" checked="checked">Yes</label>
                 <label class="radio-inline"><input type="radio" name="two_step" value="no">No</label>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
+        <div class="container_12">
+            <div class="grid_12 alpha">
                 <div class="well">
                     <strong>Reports:</strong>
                     <div class="well-lg">
@@ -47,5 +54,7 @@
                 </div>
             </div>
         </div>
+    </div>
+    </div>
     </form>
 

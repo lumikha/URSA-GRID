90b3fed92282683ddaf5a4bc792504a9916a34c2
Bethany Joyce Sibala <bethany@lumikha.co>
2016-09-15 13:14:55 +0800

    billing.php
    960.gs

--- billing.php
+++ billing.php
@@ -1,26 +1,26 @@
 <?php
-    require 'functions.php';
     require 'header.php';
 ?>
 
     <form id="billing_form">
-        <div class="row">
-            <div class="col-md-7">
+    <div class="full-width-div">
+        <div class="container_12" style="margin-top:3em;">
+            <div class="grid_6 push_1 alpha">
                 <input type="text" class="form-control" placeholder="Payment Processor ID ">
             </div>
-            <div class="col-md-3">
+            <div class="grid_3 push_1">
                 <select class="form-control">
                     <option>Trial</option>
                     <option>Active</option>
                     <option>Delinquent</option>
                 </select>
             </div>
-            <div class="col-md-2">
+            <div class="grid_3 push_1 omega">
                 <input type="number" class="form-control" placeholder="Successful Billing Cycles">
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
+        <div class="container_12">
+            <div class="grid_12 push_1 alpha">
                  <select class="form-control">
                  <option>Product 1</option>
                  <option>Product 2</option>
@@ -29,31 +29,31 @@
                  </select>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6">
+        <div class="container_12">
+            <div class="grid_6 push_1 alpha">
                 <input type="text" class="form-control" placeholder="Credit Card Masked Number">
             </div>
-            <div class="col-md-3">
+            <div class="grid_3 push_1">
                 <input type="text" class="form-control" placeholder="Credit Card Expiration Month">
             </div>
-            <div class="col-md-3">
+            <div class="grid_3 push_1 omega">
                 <input type="text" class="form-control" placeholder="Credit Card Expiration Year">
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-4">
+        <div class="container_12">
+            <div class="grid_4 push_1 alpha">
                 <input type="text" class="form-control" placeholder="Billing Address Street">
             </div>
-            <div class="col-md-3">
+            <div class="grid_3 push_1">
                 <input type="text" class="form-control" placeholder="Billing City">
             </div>
-            <div class="col-md-1">
+            <div class="grid_1 push_1">
                 <input type="text" class="form-control" placeholder="State">
             </div>
-            <div class="col-md-2">
+            <div class="grid_2 push_1">
                 <input type="text" class="form-control" placeholder="Billing Postcode">
             </div>
-            <div class="col-md-2">
+            <div class="grid_2 push_1 omega">
                 <input type="text" class="form-control" placeholder="Billing Country">
             </div>
         </div>
